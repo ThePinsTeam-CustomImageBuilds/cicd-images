36fe0549e86c8309ec3d62331cc2dfb0b39e4cfd
Andrei Jiroh Halili <ajhalili2006@gmail.com>
2021-10-05 07:06:31 +0000

    chore(scripts): :hammer: update GitLab CI setup and build scripts
    Plus, the GitLab CI configurations are also updated too in this commit.
    
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-set -ex
+set -x
 
 export CI_COMMIT_SHA=${CI_COMIT_SHA:-"$(git rev-parse HEAD)"}
+SKIP_BUILD_STEP=${SKIP_BUILD_STEP:-"0"}
 
-# TODO: Do the actual Docker build with BuildKit soon.
+# TODO: Make this work on both CI and locally when invoked with an input
 for directory in dirname $(git diff --name-only HEAD HEAD^); do
   if [[ $directory =~ docker\/.* ]] && [ -f "$PWD/$directory/build-metadata.json" ] && [ -f "$PWD/$directory/Dockerfile" ]; then
+    echo "info: Building Docker image for $PWD/$directory..."
     CI_BUILD_TIME="$(date --iso-8601=seconds)" envsubst < "$PWD/$directory/build-metadata.json" > "$PWD/$directory/build-metadata.generated.json"
 
-    echo "========== DEBUG: IMAGE METADATA =========="
+    echo "========== DEBUG: IMAGE METADATA FOR $directory =========="
     docker buildx bake --file /tmp/mkdocs-material-metadata.json --file "$PWD/$directory/build-metadata.generated.json" --push build --print
-    echo "========== DEBUG: IMAGE METADATA =========="
+    echo
     sleep 5
 
-    echo "info: Build step logs streams below..."
-    docker buildx bake --file "$PWD/$directory/build-metadata.generated.json" --push build
+    echo "========== BUILD STEP FOR $directory =========="
+    if [[ $SKIP_BUILD_STEP == "0" ]]; then
+      docker buildx bake --file "$PWD/$directory/build-metadata.generated.json" --push build
+    else
+      echo "warning: Buildkit Publish step is being skipped!"
+      docker buildx bake --file "$PWD/$directory/build-metadata.generated.json"
+    fi
+    echo
   else
-    echo "warning: Unsupported image path, please make sure $directory has either Dockerfile, build-metadata.json, or both."
+    echo "warning: Unsupported image path, please make sure $directory has either Dockerfile, build-metadata.json, or both. SKipping..."
+    echo
   fi
 done
--- scripts/gitlab-cicd-setup.sh
+++ scripts/gitlab-cicd-setup.sh
@@ -17,14 +17,20 @@ export CI_REGISTRY=${CI_REGISTRY:-"registry.gitlab.com"} \
     CI_SERVER_PORT=${CI_SERVER_PORT:-"443"}
 
 # Step 1: Download Docker CLI from upstream
+echo "==> Installing Docker CLI from download.docker.com..."
 curl -fsSLO "https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKERVERSION}.tgz"
 tar xzvf "docker-${DOCKERVERSION}.tgz" --strip 1 -C /usr/local/bin docker/docker
 rm "docker-${DOCKERVERSION}.tgz"
 
 # Step 2: Install Doppler
-[ "$SKIP_DOPPLER_STEP" == "0" ] && curl -Ls --tlsv1.2 --proto "=https" --retry 3 https://cli.doppler.com/install.sh | sh
+if [ "$SKIP_DOPPLER_STEP" == "0" ]; then
+  curl -Ls --tlsv1.2 --proto "=https" --retry 3 https://cli.doppler.com/install.sh | sh
+else
+  echo "warning: Doppler CLI install step is being skipped!"
+fi
 
 # Step 3: Install Hadolint and ShellCheck
+echo "==> Installing ShellCheck and Hadolint"
 apk add shellcheck
 wget "https://github.com/hadolint/hadolint/releases/download/v${HADOLINT_VERSION}/hadolint-Linux-x86_64" -O /usr/local/bin/hadolint
 chmod +x /usr/local/bin/hadolint
@@ -35,5 +41,10 @@ else
   echo "warning: Login step was being skipped!"
 fi
 
-# Step 4: Setup multiarch builds
-docker buildx create --name thepinsteam-glcicd-custom-image-builder --use
+# Step 4: Setup multiarch builds ahead of time.
+echo "==> Setting up QEMU..."
+docker run --rm --privileged tonistiigi/binfmt:latest --install all
+echo "==> Setting up Docker buildx..."
+docker buildx create --name thepinsteam-glcicd-custom-image-builder --use --driver docker-container --buildkitd-flags --allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host
+docker buildx inspect --bootstrap --builder thepinsteam-glcicd-custom-image-builder
+docker buildx install
